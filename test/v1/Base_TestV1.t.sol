2c1fb1aa5329452ffeef81d156ed0a7c29ab5d34
aizen <tarunsanjayrao@gmail.com>
2024-03-07 18:33:00 +0530

    chore: forge init

---
+++ test/v1/Base_TestV1.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+abstract contract Base_TestV1 is Test {
+    HorseStore public horseStore;
+
+    function setUp() public virtual {
+        horseStore = new HorseStore();
+    }
+
+    function testReadValue() public {
+        uint256 horseCount = horseStore.readNumberOfHorses();
+        assertEq(horseCount, 0)
+    }
+}
